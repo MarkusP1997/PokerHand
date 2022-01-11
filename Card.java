4caa135490527ed2d9a59be77d241e0ef81c646d
MarkusP1997 <96255999+MarkusP1997@users.noreply.github.com>
2022-01-11 10:33:51 +0100

    reloaded Card class

---
+++ Card.java
@@ -0,0 +1,14 @@
+public class Card {
+    private int suit;
+    private int value;
+    public Card (int s, int v) {
+        suit = s;
+        value = v;
+    }
+    int getS () {
+        return suit;
+    }
+    int getV () {
+        return value;
+    }
+}
